a40388d46f45d595ade1354089e2e41f3f0488d2
math <math@informatik.uni-leipzig.de>
2012-08-11 17:31:12 +0200

    [ADD] Moved lineGuidedSlice module from framework here.

---
+++ Unmaintained/src/WToolkit.cpp
@@ -0,0 +1,15 @@
+#include <boost/shared_ptr.hpp>
+
+#include <core/kernel/WModule.h>
+
+#include "lineGuidedSlice/WMLineGuidedSlice.h"
+
+#include "WToolkit.h"
+
+// This file's purpose is to provide a list of modules as entry point for OpenWalnut's module loader.
+// Add your modules here. If you miss this step, OpenWalnut will not be able to load your modules.
+extern "C" void WLoadModule( WModuleList& m ) // NOLINT
+{
+    m.push_back( boost::shared_ptr< WModule >( new WMLineGuidedSlice ) );
+}
+
