1141adfe905d48811b69ec309e488c72bedc4caf
math <math@informatik.uni-leipzig.de>
2012-12-17 12:20:21 +0100

    [CHANGE] Made toolbox compile. This is done by adapting code of SETUP_MODULE to work with CUDA_ADD_LIBRARY and other CUDA related stuff.

--- FiberClusteringToolbox/src/detTractClustering/WMDetTractClusteringCudaInterface.cpp
+++ FiberClusteringToolbox/src/detTractClustering/WMDetTractClusteringCudaInterface.cpp
@@ -77,8 +77,7 @@ bool initDLtTableCuda( boost::shared_ptr< WMatrixSym > dLtTable,
     ProgressWrapper *progress = new ProgressWrapper( &progressData );
 
     float* distmat = new float[ntracts * ntracts];
-    // TODO(math): This is disabled as CUDA code won't compile anymore with the new CMakeProjects
-    bool result; // = distCuda( distmat, ncoords, coords, ntracts, offsets, lengths, proximity_threshold, progress );
+    bool result = distCuda( distmat, ncoords, coords, ntracts, offsets, lengths, proximity_threshold, progress );
 
     // copy results to OpenWalnut structure
     for( size_t q = 0; q < ntracts; ++q )
