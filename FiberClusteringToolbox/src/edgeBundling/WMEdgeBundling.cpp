a0f0a198522c29f46dfb9ee4ad738a7068fc47e2
math <math@informatik.uni-leipzig.de>
2013-03-18 16:20:31 +0100

    [ADD] Very simple (pointless) OpenCL usage.

--- FiberClusteringToolbox/src/edgeBundling/WMEdgeBundling.cpp
+++ FiberClusteringToolbox/src/edgeBundling/WMEdgeBundling.cpp
@@ -23,8 +23,13 @@
 //---------------------------------------------------------------------------
 
 #include <string>
+#include <vector>
+
+#define __CL_ENABLE_EXCEPTIONS
+#include "cl.hpp"
 
 #include "core/dataHandler/WDataSetFibers.h"
+#include "core/dataHandler/WDataSetScalar.h"
 #include "core/kernel/WKernel.h"
 #include "core/kernel/WModuleInputData.h"
 
@@ -57,6 +62,7 @@ const std::string WMEdgeBundling::getDescription() const
 void WMEdgeBundling::connectors()
 {
     m_fibersIC = WModuleInputData< WDataSetFibers >::createAndAdd( shared_from_this(), "fibers", "The line data to bundle." );
+    m_maskIC   = WModuleInputData< WDataSetScalar >::createAndAdd( shared_from_this(), "mask", "Optional mask, specifying valid positions for segments" );
     m_fibersOC = WModuleOutputData< WDataSetFibers >::createAndAdd( shared_from_this(), "bundledFibers", "The bundled line data." );
 
     WModule::connectors();
@@ -64,6 +70,26 @@ void WMEdgeBundling::connectors()
 
 void WMEdgeBundling::properties()
 {
+    m_stiffness      = m_properties->addProperty( "Stiffness", "Variability of fiber sample points", 0.5 );
+    m_stiffness->setMin( 0.0 );
+    m_stiffness->setMax( 1.0 );
+    m_fixedEndings   = m_properties->addProperty( "Fixed Ending", "If true, endings of fibers will not change positions", true );
+    m_maxCurvature   = m_properties->addProperty( "Max curvature", "Curvature Threshold as constraint for moving positions", 1.0 );
+    m_maxCurvature->setMin( 0.0 );
+    m_maxCurvature->setMax( 1.0 );
+    m_curveSegments  = m_properties->addProperty( "Curvature Segments", "How many segments used for Max Curvature threshold", 1 );
+    m_curveSegments->setMin( 0 );
+    m_minDistance    = m_properties->addProperty( "Min Point Distance", "How close may edges packed", 0.0 );
+    m_minDistance->setMin( 0.0 );
+    m_minDistance->setMax( 1.0 );
+    m_maxExtension   = m_properties->addProperty( "Max Extension", "Percentage how much fibers may be elongated", 100.0 );
+    m_maxExtension->setMin( 0.0 );
+    m_maxContraction = m_properties->addProperty( "Max Contraction", "Percentage how much fibers may be shortened", 50.0 );
+    m_maxContraction->setMin( 0.0 );
+    m_maxContraction->setMax( 100.0 );
+    m_maxRadius      = m_properties->addProperty( "Attraction Radius", "Only segments within this radius will contribute to attraction", 0.0 );
+    m_maxRadius->setMin( 0.0 );
+
     WModule::properties();
 }
 
@@ -71,11 +97,35 @@ void WMEdgeBundling::requirements()
 {
 }
 
+void WMEdgeBundling::initOpenCL()
+{
+    cl_int                      ret;
+    std::string value;
+    std::vector< cl::Platform > platforms;
+    cl::Platform::get( &platforms );
+    for( std::vector< cl::Platform >::const_iterator cit = platforms.begin(); cit != platforms.end(); ++cit )
+    {
+        cit->getInfo( CL_PLATFORM_PROFILE, &value );
+        debugLog() << "CL_PLATFORM_PROFILE: " << value;
+        cit->getInfo( CL_PLATFORM_VERSION, &value );
+        debugLog() << "CL_PLATFORM_VERSION: " << value;
+        cit->getInfo( CL_PLATFORM_NAME , &value );
+        debugLog() << "CL_PLATFORM_NAME: " << value;
+        cit->getInfo( CL_PLATFORM_VENDOR , &value );
+        debugLog() << "CL_PLATFORM_VENDOR: " << value;
+        cit->getInfo( CL_PLATFORM_EXTENSIONS, &value );
+        debugLog() << "CL_PLATFORM_EXTENSIONS: " << value;
+    }
+}
+
 void WMEdgeBundling::moduleMain()
 {
     // get notified about data changes
     m_moduleState.setResetable( true, true );
     m_moduleState.add( m_fibersIC->getDataChangedCondition() );
+    m_moduleState.add( m_maskIC->getDataChangedCondition() );
+
+    initOpenCL();
 
     ready();
 
@@ -89,6 +139,14 @@ void WMEdgeBundling::moduleMain()
         if( m_shutdownFlag() )
         {
             break;
+        }
+
+        WDataSetScalar::SPtr mask = m_maskIC->getData();
+        WDataSetFibers::SPtr fibers = m_fibersIC->getData();
+
+        if( !fibers )
+        {
+            continue;
         }
     }
 }
