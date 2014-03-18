a950e3fd98c0b8b48aa4d94415073324d8d11ac1
math <math@informatik.uni-leipzig.de>
2014-03-18 17:35:14 +0100

    [FORGOT] to add two strategy files

---
+++ FiberClusteringToolbox/src/fiberColoring/WBoyGlobalColoring.h
@@ -0,0 +1,56 @@
+//---------------------------------------------------------------------------
+//
+// Project: OpenWalnut ( http://www.openwalnut.org )
+//
+// Copyright 2009 OpenWalnut Community, BSV@Uni-Leipzig and CNCF@MPI-CBS
+// For more information see http://www.openwalnut.org/copying
+//
+// This file is part of OpenWalnut.
+//
+// OpenWalnut is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenWalnut is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OpenWalnut. If not, see <http://www.gnu.org/licenses/>.
+//
+//---------------------------------------------------------------------------
+
+#ifndef WBOYGLOBALCOLORING_H
+#define WBOYGLOBALCOLORING_H
+
+#include <core/common/WObjectNDIP.h>
+
+#include "WColoring_I.h"
+
+class WBoyGlobalColoring : public WObjectNDIP< WColoring_I >
+{
+public:
+    /**
+     * Constructor.
+     */;
+    WBoyGlobalColoring();
+
+    /**
+     * Destructor.
+     */
+    virtual ~WBoyGlobalColoring();
+
+    /**
+     * Implements edge bundling on CPU.
+     *
+     * \copydetails WEdgeBundlingInterface::operator()()
+     */
+    virtual WDataSetFibers::SPtr operator()( WProgress::SPtr progress, WBoolFlag const &shutdown, WDataSetFibers::SPtr fibers );
+protected:
+private:
+    WDataSetFibers::ColorArray m_boyColors;
+};
+
+#endif  // WBOYGLOBALCOLORING_H
